eccb9fabe490b6bee1caf265ac1b4e3be7858017
antondtran <antonduytran@gmail.com>
2023-03-31 16:32:17 -0700

    created service interface and class implementation

---
+++ src/main/java/com/example/employeecrud/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.employeecrud.service;
+
+import com.example.employeecrud.entity.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+
+    public Optional<Employee> findById(int id);
+    public List<Employee> findAll();
+    public void save(Employee employee);
+    public void deleteById(int id);
+}
---
+++ src/main/java/com/example/employeecrud/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.employeecrud.service;
+
+import com.example.employeecrud.entity.Employee;
+import com.example.employeecrud.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Optional<Employee> findById(int id) {
+        return employeeRepository.findById(id);
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return employeeList;
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
