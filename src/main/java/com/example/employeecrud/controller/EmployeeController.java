5321c9deb2a11878e029abea410bbe2d53577577
antondtran <antonduytran@gmail.com>
2023-03-31 17:01:08 -0700

    added rest endpoints

--- src/main/java/com/example/employeecrud/controller/EmployeeController.java
+++ src/main/java/com/example/employeecrud/controller/EmployeeController.java
@@ -5,9 +5,7 @@ import com.example.employeecrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +24,7 @@ public class EmployeeController {
     public String listAllEmployees(Model model){
         List<Employee> employeeList = employeeService.findAll();
         model.addAttribute("employee", employeeList);
-        return "employee";
+        return "employees/list-employees";
     }
 
     @GetMapping("/showFormForAdd")
@@ -34,8 +32,31 @@ public class EmployeeController {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
 
-        return "employee";
+        return "employees/employee-form";
     }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model){
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("employee") Employee employee){
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteById(@RequestParam("employeeId") int id){
+        employeeService.deleteById(id);
+
+        return "redirect:/employees/list";
+    }
+
 
 
 
